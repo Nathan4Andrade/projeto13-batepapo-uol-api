8e6ed0840d78bda689a7b202922c9cee6c85b31c
Nathan4Andrade <nathanandrade20@gmail.com>
2023-06-27 13:09:17 -0300

    feat: post and get calls

--- src/app.js
+++ src/app.js
@@ -5,5 +5,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/participants", (req, res) => {});
+
+app.get("/participants", (req, res) => {});
+
+app.post("/messages", (req, res) => {});
+
+app.get("/messages", (req, res) => {});
+
+app.post("/status", (req, res) => {});
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
