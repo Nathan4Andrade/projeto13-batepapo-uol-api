1e33e61855027753a858217687275206deb0df25
Nathan4Andrade <nathanandrade20@gmail.com>
2023-07-03 13:59:20 -0300

    feat: post participants e messages

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dayjs from "dayjs";
+import Joi from "joi";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+const currentTime = dayjs().format("HH:mm:ss");
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
@@ -18,49 +20,120 @@ mongoClient
   .then(() => (db = mongoClient.db()))
   .catch((err) => console.log(err.message));
 
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
   const { name } = req.body;
+
+  const schemaParticipant = Joi.object({
+    name: Joi.string().required(),
+  });
+
+  const validation = schemaParticipant.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
 
   if (!name) {
     res.status(422).send("Todos os campos são obrigatórios!");
     return;
   }
 
-  const newParticipant = {
-    name,
-    lastStatus: Date.now(),
-  };
+  try {
+    const participant = await db
+      .collection("participants")
+      .findOne({ name: name });
+    if (participant) return res.status(409).send("Esse nome já existe!");
 
-  db.collection("participants")
-    .insertOne(newParticipant)
-    .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    await db.collection("participants").insertOne({
+      name,
+      lastStatus: Date.now(),
+    });
 
-  const newMessage = {
-    from: name,
-    to: "Todos",
-    text: "entra na sala...",
-    type: "status",
-    time: dayjs().format("HH:mm:ss"),
-  };
-
-  db.collection("messages")
-    .insertOne(newMessage)
-    .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: currentTime,
+    });
+    res.sendStatus(201);
+  } catch {
+    res.sendStatus(500);
+  }
 });
 
-app.get("/participants", (req, res) => {
-  db.collection("participants")
-    .find()
-    .toArray()
-    .then((participants) => res.send(participants))
-    .catch((err) => res.status(500).send(err.message));
+app.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-app.post("/messages", (req, res) => {});
+app.post("/messages", async (req, res) => {
+  const user = req.headers.user;
 
-app.get("/messages", (req, res) => {});
+  const participant = await db
+    .collection("participants")
+    .findOne({ name: user });
+  if (!participant)
+    return res.status(422).send("Esse usuário não existe ou foi desconectado!");
+
+  const schemaMessage = Joi.object({
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().valid("message", "private_message").required(),
+  });
+
+  const validation = schemaMessage.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  const message = {
+    from: user,
+    ...req.body,
+    time: currentTime,
+  };
+
+  try {
+    await db.collection("messages").insertOne(message);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(422);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const { user } = req.headers;
+  const { limit } = req.query;
+
+  try {
+    const showMessages = await db
+      .collection("messages")
+      .find({ $or: [{ from: user }, { to: { $in: ["Todos", user] } }] })
+      .toArray();
+
+    if (limit) {
+      const limitedSchema = Joi.number().min(1);
+      const { error } = limitedSchema.validate(limit);
+      if (error) {
+        return res.status(422).send(error.message);
+      }
+      return res.send(showMessages.slice(-limit));
+    }
+
+    res.send(showMessages);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.post("/status", (req, res) => {});
 
